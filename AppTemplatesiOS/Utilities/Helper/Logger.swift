537e4fad9d2fd7e3fb65ae542de5f507b88583f5
rezaulislam <mrezaulislam.t@gmail.com>
2025-07-11 13:13:23 +0600

    Added Logger to simple log

--- AppTemplatesiOS/Utilities/Helper/Logger.swift
+++ AppTemplatesiOS/Utilities/Helper/Logger.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+struct Logger {
+    enum LogLevel: String {
+        case info = "INFO"
+        case debug = "DEBUG"
+        case warning = "WARNING"
+        case error = "ERROR"
+    }
+
+    static func log(_ message:
+                    Any, level: LogLevel = .info, file: String = #file, function: String = #function, line: Int = #line) {
+        let fileName = (file as NSString).lastPathComponent
+        let timestamp = Date().formatted()
+
+        print("[\(timestamp)] [\(level.rawValue)] [\(fileName):\(line) - \(function)] \(message)")
+    }
+}
+
+extension Date {
+    func formatted() -> String {
+        let formatter = DateFormatter()
+        if #available(iOS 16, *) {
+            formatter.locale = Locale()
+        } else {
+            // Fallback on earlier versions
+            formatter.locale = .autoupdatingCurrent
+        }
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter.string(from: self)
+    }
+}
+
