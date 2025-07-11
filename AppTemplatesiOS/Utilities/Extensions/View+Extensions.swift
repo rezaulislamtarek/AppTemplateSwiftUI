49f5de414554ef0f772d93501b70347c1d70b8ba
rezaulislam <mrezaulislam.t@gmail.com>
2025-07-11 14:37:46 +0600

    Add view extension

--- AppTemplatesiOS/Utilities/Extensions/View+Extensions.swift
+++ AppTemplatesiOS/Utilities/Extensions/View+Extensions.swift
@@ -9,8 +9,24 @@ import SwiftUI
 
 extension View{
     
+    func fullHeightWidth() -> some View {
+        frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
     
-
+    func fullwidth(alignment: Alignment = .center) -> some View {
+        frame(maxWidth: .infinity, alignment: alignment)
+    }
+    
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+    
+    func onTapAsButton(_ action: @escaping () -> Void) -> some View {
+         Button(action: action) {
+            self
+        }
+    }
+    
     func disableSwipeBack( ) -> some View {
         self.onAppear {
             NavState.shared.swipeEnabled = false
@@ -20,7 +36,41 @@ extension View{
         }
     }
     
+    func showBackgroundOverlay(_ isShow: Bool) -> some View {
+        self.overlay {
+            Color.black.opacity(0.4)
+                .ignoresSafeArea(edges: .all)
+                .opacity( isShow ? 1 : 0)
+        }
+    }
+    
     func hideKeyboard() {UIApplication.shared.sendAction( #selector( UIResponder.resignFirstResponder ), to: nil, from: nil, for: nil)}
+    
+    func backToolBar(_ title : String? = "",  _ action : @escaping ()-> Void ) -> some View{
+        self.navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigation) {
+                    Button {
+                        action()
+                    } label: {
+                        Image(systemName: "arrow.backward")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                            .foregroundStyle(.black)
+                    }
+                    
+                }
+                
+                ToolbarItem(placement: .principal) {
+                    Text(title ?? "")
+                        .fontBold(20)
+                    
+                }
+            }
+    }
 }
 
 
+let height = UIScreen.main.bounds.height
+let width = UIScreen.main.bounds.width
